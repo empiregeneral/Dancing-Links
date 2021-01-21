0dbf3dab5ea359fe92e8f87609639c618ff88dfe
empiregeneral <13777470884@qq.com>
2021-01-21 23:10:38 +0800

    [MOD] cell

--- src/main/java/org/canmo/ColumnHeader.java
+++ src/main/java/org/canmo/ColumnHeader.java
@@ -1,4 +1,19 @@
 package org.canmo;
 
+/**
+ * Column headers record the name of the column and the number of rows that
+ * satisfy this column. The names are provided by the application and can
+ * be anything. The size is used for the heuristic for picking the next
+ * column to explore.
+ */
 public class ColumnHeader<ColumnName> extends Node<ColumnName> {
+
+    int size;
+    ColumnName name;
+
+    public ColumnHeader(ColumnName n,int s) {
+        name = n;
+        size = s;
+        head = this;
+    }
 }
--- src/main/java/org/canmo/Node.java
+++ src/main/java/org/canmo/Node.java
@@ -1,9 +1,45 @@
 package org.canmo;
 
-/**
- * @author Administrator
+
+ /**
+ * A cell in the table with up/down and left/right links that form doubly
+ * linked lists in both directions. It also includes a link to the column
+ * head.
  */
 public class Node<ColumnName> {
+    /** Left */
+    Node<ColumnName> left;
+    /** Right */
+    Node<ColumnName> right;
+    /** Upper */
+    Node<ColumnName> up;
+    /** Down */
+    Node<ColumnName> down;
 
+    ColumnHeader<ColumnName> head;
 
-}
+    public Node(Node<ColumnName> l, Node<ColumnName> r,
+                Node<ColumnName> u, Node<ColumnName> d,
+                ColumnHeader<ColumnName> h) {
+        this.left = l;
+        this.right = r;
+        this.up = u;
+        this.down = d;
+        this.head = h;
+    }
+
+    public Node() {
+        this(null, null, null, null, null);
+    }
+
+     @Override
+     public String toString() {
+         return "Node{" +
+                 "left=" + left.toString() +
+                 ", right=" + right.toString() +
+                 ", up=" + up.toString() +
+                 ", down=" + down.toString() +
+                 ", head=" + head.toString() +
+                 '}';
+     }
+ }
